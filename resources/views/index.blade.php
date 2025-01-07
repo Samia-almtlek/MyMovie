21ed76b674aa77c3c4d65b3f0dd7fea308f4efc6
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-07 17:02:02 +0100

    Add the hero section

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <!--Hero Section-->
+
+    <div class="bg-image d-flex flex-column">
+        <div class="flex-grow-1 d-flex align-items-center justify-content-center text-center">
+            <div>
+                <h1>Welcome to your hub for movie news, reviews, and ratings. Join a community that loves cinema as much
+                    as you do!My Movie .. Start the new movie world </h1>
+                <a href="/" class="btn">Start Reading</a>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
