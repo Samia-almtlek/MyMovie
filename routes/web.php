5384487733b0107fdf6ca8ecb78c1561d7cf5274
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-07 16:53:53 +0100

    Add index function from PageController in routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PagesController::class, 'index']);
+Route::get('/',[PagesController::class, 'index']);
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
