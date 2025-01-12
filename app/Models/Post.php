fd7f370e3d4f2d5876e870a1fb76e8d7b98ccd1e
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-12 18:50:41 +0100

    Adding a table for posts in the database.

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+use HasFactory;
+    protected $fillable=[
+        'title' ,'slug','description' ,'my_review','image_path','genre','release_year','user_id'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
