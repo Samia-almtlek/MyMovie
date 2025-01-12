a557607ee36a584ed17903a93d46b33b67fc2b1e
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-12 18:47:23 +0100

    add function create to create a post page and show function to fetch data from database and show it in the blog page after create a post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        return view('blog.index'); 
+        
+        return view('blog.index')
+        ->with('posts',Post::get());
     }
 
     /**
@@ -19,7 +21,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
@@ -27,7 +29,34 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+
+      $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'image' => 'required|mimes:jpeg,png,jpg,gif|max:5048',  // image validation
+            'genre' => 'required|max:100',
+            'release_year' => 'required|integer|min:1900|max:' . date('Y'),
+            'my_review' => 'required|max:1000', // Optional field
+        ]); 
+        $slug =Str::slug($request->title, '-');
+
+        $newImageName=uniqid(). '-' .$slug .'.' .$request->image->extension();
+        $request->image->move(public_path('images'), $newImageName);
+       
+        Post::create([
+            'title'=>$request->input('title'),
+            'description'=>$request->input('description'),
+            'my_review'=>$request->input('my_review'),
+            'genre'=>$request->input('genre'),
+            'release_year'=>$request->input('release_year'),
+            'slug'=>$slug,
+            'image_path'=>$newImageName,
+            'user_id'=> auth()->user()->id
+        ]); 
+
+        return redirect('/blog')->with('success', 'Post created successfully!');
+        
     }
 
     /**
