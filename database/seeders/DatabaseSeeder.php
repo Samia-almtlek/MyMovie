8a646e7c7c7ca18942da7dc8bc9238b750cc0d2b
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-12 19:08:38 +0100

    Add a default user and admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create a default admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@ehb.be',
+            'password' => Hash::make('Password!321'), // Encrypted password
+            'is_admin' => true, // Mark as admin
+        ]);
 
-        User::factory()->create([
+        // Create a test user
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('Password123'), // Encrypted password
+            'is_admin' => false, // Regular user
         ]);
     }
 }
