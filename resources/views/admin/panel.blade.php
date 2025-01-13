5b27b79a64fa300f779eb4d0d31d23c4f29d49b4
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-13 18:13:16 +0100

    Add panel page that make admin make a post of mange an user

---
+++ resources/views/admin/panel.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="container text-center mt-5">
+        <h1 class="text-danger">Admin Panel</h1>
+        <p class="text-secondary">Welcome to the admin panel! Here you can manage users, posts, and more.</p>
+
+        <div class="mt-4">
+            <a href="/admin/users" class="btn btn-primary">Manage Users</a>
+            <a href="/blog/create" class="btn btn-success">Add New Post</a>
+        </div>
+    </div>
+</x-app-layout>
