ba6a6bc4a69132bbf8a1573cc84aa6d072b56682
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-13 02:51:24 +0100

    Add the CRUD method in the PostController to enable the admin to add, edit, and delete posts.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -55,39 +55,80 @@ class PostsController extends Controller
             'user_id'=> auth()->user()->id
         ]); 
 
-        return redirect('/blog')->with('success', 'Post created successfully!');
+        return redirect('/blog')->with('PostSuccess', 'Post created successfully!');
         
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($slug)
+    
     {
-        //
+        return view('blog.show')
+        ->with('post',Post::where('slug', $slug)->first());
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($slug)
     {
-        //
+        $post = Post::where('slug', $slug)->first();
+
+    // Check if the user is admin and the owner of the post
+    if (!auth()->user()->is_admin || auth()->user()->id != $post->user_id) {
+        abort(403, 'Unauthorized action.');
     }
+
+    return view('blog.edit')->with('post', $post);
+}
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $slug)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'image' => 'required|mimes:jpeg,png,jpg,gif|max:5048',  // image validation
+            'genre' => 'required|max:100',
+            'release_year' => 'required|integer|min:1900|max:' . date('Y'),
+            'my_review' => 'required|max:1000', // Optional field
+        ]); 
+        
+
+        $newImageName=uniqid(). '-' .$slug .'.' .$request->image->extension();
+        $request->image->move(public_path('images'), $newImageName);
+       
+        
+        Post::where('slug',$slug)
+        ->update([
+            'title'=>$request->input('title'),
+            'description'=>$request->input('description'),
+            'my_review'=>$request->input('my_review'),
+            'genre'=>$request->input('genre'),
+            'release_year'=>$request->input('release_year'),
+            'slug'=>$slug,
+            'image_path'=>$newImageName,
+            'user_id'=> auth()->user()->id
+        ]); 
+
+        return redirect()->route('blog.show', $slug)->with('EditSuccess', 'Successfully updated');
+        
     }
+    
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($slug)
     {
-        //
+        Post::where('slug',$slug)->delete();
+        return redirect('/blog')->with('DeleteSuccess', 'Post Deleted successfully!');
+
+
     }
 }
