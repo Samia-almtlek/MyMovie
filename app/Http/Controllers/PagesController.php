d388a323e40ffd1deb927b2c2fb2742c7c8015e5
Samia-almtlek <samia.almoutlak@gmail.com>
2025-01-14 22:51:30 +0100

    import String support

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\Post;
+
+
+
+
 
 class PagesController extends Controller
 {
@@ -12,6 +16,7 @@ class PagesController extends Controller
      */
     public function index()
     {
+        
         $latestPost = Post::latest()->first(); // جلب آخر بوست
         return view('index', compact('latestPost')); // إرسال البوست إلى الصفحة
     }
